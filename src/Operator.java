4631b7e7b54b97df6ac59f51dc7dd9925f3015b7
InoKotaro <ino.kota0602@gmail.com>
2023-03-10 17:44:39 +0900

    [add]キャスト

--- src/Operator.java
+++ src/Operator.java
@@ -2,6 +2,8 @@ public class Operator {
   
  public static void main(String[] args) throws Exception {
 
+  //============================演算子について================================
+
   //①算術演算子
   // 7 / 2 → 3 //余りは非表示
   // 7 % 2 → 1 //余りだけ表示
@@ -11,9 +13,16 @@ public class Operator {
 
   //③インクリメント演算子・デクリメント演算子
   // a++ → a = a + 1 または a += 1 と同じ 
-  //======================================================
 
-  int a = Integer.parseInt(args[0]);
+  //④関係演算子
+  // a >= 0 で a が0以上であれば true が返される(boolean型で返ってくる)
+
+  //⑤論理演算子
+  // (a >= 0) && (b >= 0) 関係演算子同士を評価して、結果をboolean型で返す
+  // 「&&」が「且つ」、「||」が「どっちかが」
+  //========================================================================
+
+  int a = Integer.parseInt(args[0]); //カッコ内に指定した文字列をint型に変換されて変数aが整数に置き換わる
   int b = Integer.parseInt(args[1]);
 
   System.out.println("a = " + a + ",b = " + b);
@@ -34,6 +43,9 @@ public class Operator {
   a % b = 2
   a >= 0 : true
   (a >= 0) && (b >= 0) : true //「&&」や「||」は真偽値で返される
+  ターミナルで引数に数値ではなく文字を入力すると
+  「Integer.parseInt」で数値に変換したいのに文字が引数になってる胸の
+  「NumberFormatException」というエラーになる
   */
   }
 

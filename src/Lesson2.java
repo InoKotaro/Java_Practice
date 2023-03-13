92fc5adb498e441c0f0d74c2e9ba5ccd70e6b829
InoKotaro <ino.kota0602@gmail.com>
2023-03-13 20:23:56 +0900

    [add]演習問題

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -31,14 +31,40 @@ public class Lesson2 {
     // |
     // V
 
-    int radius = Integer.parseInt(args[0]);
-    double pi = 3.141592;
-    double area;
+    // int radius = Integer.parseInt(args[0]);
+    // double pi = 3.141592;
+    // double area;
 
-    area = radius * radius * pi;
+    // area = radius * radius * pi;
 
-    System.out.println("半径: " + radius);
-    System.out.println("面積: " + (Math.floor(area * 100))/100);
+    // System.out.println("半径: " + radius);
+    // System.out.println("面積: " + (Math.floor(area * 100))/100);
+
+    //=========================================================
+    // int a = Integer.parseInt(args[0]);
+    // int b = Integer.parseInt(args[1]);
+
+    // if (a > b){
+    //   System.out.println("大きいのは" + a + "です");
+    // }else{
+    //   System.out.println("大きいのは" + b + "です");
+    // }
     
+    //=========================================================
+    // int count = 1;
+    // while(count <= 10){
+    //   System.out.println(count + "回目: while文演習問題");
+    //   count++;
+    // }
+
+    //=========================================================
+    //int a = Integer.parseInt(args[0]);
+
+    // for(int i = 0; i < args.length; i++){
+    //   System.out.println(args);
+    // }
+    for(String s:args){
+      System.out.println(s);
+      }
   }
 }

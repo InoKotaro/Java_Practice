c9d53382d56550f5f2873ae3656599471c40062e
InoKotaro <ino.kota0602@gmail.com>
2023-03-18 17:58:12 +0900

    [add]オブジェクト指向(実行方法)

--- src/NoObject.java
+++
@@ -1,9 +0,0 @@
-public class NoObject {
-  public static void main(String[] args) throws Exception {
-    //============オブジェクト指向で書くには==================
-    //実行用クラスと設計図クラスが必要
-    //実行用クラス: mainメソッドを記述
-    //設計図クラス: 属性(変数)と操作(メソッド)を記述
-
-  }
-}

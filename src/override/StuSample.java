adf88024e4289338eacb61a2ef38076cbea57e96
InoKotaro <ino.kota0602@gmail.com>
2023-04-10 21:34:11 +0900

    [add]オーバーライド

---
+++ src/override/StuSample.java
@@ -0,0 +1,9 @@
+public class StuSample {
+  public static void main(String[] args) throws Exception {
+    Student2 stu = new Student2();
+
+    stu.setName("諸橋");
+    stu.setStuNo(1);
+    stu.display();
+  }
+}
